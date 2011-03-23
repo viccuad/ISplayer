a2bafc215f01991a561fef61b15a89ac5654d12e
Julian Martin Diez <julianmdm@gmail.com>
2011-03-23 13:53:48 +0000

    La primera, aver a cuantas llegamos

---
+++ src/Principal.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+
+/**
+ * @author Administrator
+ *
+ */
+public class Principal {
+	static String s = "Hola mierda";
+	
+	public static void main (String args[]) {
+		System.out.println(s);
+	}
+}
