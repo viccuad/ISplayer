82adbd78dd742d721f62aaecae5300a9189b6beb
Carlos Moya Ruiz <carlosmoyaruiz@gmail.com>
2011-04-03 18:43:31 +0000

    rhyfdh

---
+++ src/Test/TestCancion.java
@@ -0,0 +1,18 @@
+package Test;
+
+import is2011.reproductor.modelo.CancionMP3;
+
+public class TestCancion {
+
+	public TestCancion(){
+		
+	}
+	public static void main(String[] args){
+		
+		System.out.println("Test");
+		//CancionMP3 c = new CancionMP3("/Recursos/1.mp3");
+		//System.out.println(c.getNumeroFrames());
+		//System.out.println(c.getTrackLength());
+		//System.out.println(c.getPath());
+	}
+}
