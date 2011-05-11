e02f1f2828b8dac1af174b1436da6172e75fbcf6
Carlos Moya Ruiz <carlosmoyaruiz@gmail.com>
2011-05-11 11:12:35 +0000

    juli

--- src/is2011/reproductor/modelo/ListaReproduccion.java
+++ src/is2011/reproductor/modelo/ListaReproduccion.java
@@ -235,8 +235,6 @@ public class ListaReproduccion {
 	}
 	
 	
-	
-	
 	/**
 	 * Carga la lista de reproducción XML recibiendo la ruta en la que se ubica.
 	 * @param pathYfichero ruta absoluta al fichero XML de la lista de reproducción
